3523a3443ec6227996c01c5de30c10083fba6e49
Scypio95 <43706863+Scypio95@users.noreply.github.com>
2019-02-07 00:17:57 +0100

    Create PrettyPrintBlockBehavior2.py
    updating pretty print

---
+++ data/config/PrettyPrintBlockBehavior2.py
@@ -0,0 +1,80 @@
+#Works only in python 3
+import xml.etree.ElementTree
+
+global SectorRange
+SectorRange = 2000
+
+def GetBlockBehaviorConfigWeapons():
+	Tree = xml.etree.ElementTree.ElementTree()
+	Tree.parse("blockBehaviorConfig.xml")
+	WeaponsList = "Cannon", "Missile"
+	for i in WeaponsList:
+		Xpath = ".//" + i
+		print ("**" + i + "**```md")
+		GetWeaponStats(Tree, Xpath)
+		print ("```")
+	return
+	
+def GetWeaponStats(Tree, Xpath):
+	XpathBase = Xpath + "/BasicValues"
+	# Part for the damage
+	Links = Tree.iterfind(XpathBase + "/Damage")
+	for Iterations in Links:
+		try:
+			Iterations.attrib['version'] # will throw an exception for the 2nd iteration where there is no version attribute
+		except:
+			BaseDamage = Iterations.text
+	# Part for the power consumption
+	Links = Tree.find(XpathBase + "/ReactorPowerConsumptionResting")
+	BasePowerConsumptionResting = Links.text
+	Links = Tree.find(XpathBase + "/ReactorPowerConsumptionCharging")
+	BasePowerConsumptionCharging = Links.text
+	# Part for the distance
+	Links = Tree.find(XpathBase + "/Distance")
+	BaseRange = Links.text
+	# Part for the speed
+	Links = Tree.find(XpathBase + "/Speed")
+	BaseSpeed = Links.text
+	# Part for the reload time
+	Links = Tree.find(XpathBase + "/ReloadMs")
+	BaseReload = Links.text
+	# Pretty printing base values
+	print ("< Base weapon >")
+	print ("[Power consumption when resting][" + str(BasePowerConsumptionResting) + "]")
+	print ("[Power consumption when charging][" + str(BasePowerConsumptionCharging) + "]")
+	print ("[Damage per shot][" + str(BaseDamage) + "]")
+	print ("[Reload time of][" + str(float(BaseReload)/1000) + "]")
+	print ("[Damage per seconds of][" + str((float(BaseDamage)*1000)/(float(BaseReload))) + "]")
+	print ("[Range of][" + str(float(BaseRange) * SectorRange) + "]")
+	print ("[Projectile speed of][" + str(BaseSpeed) + "]")
+	
+	# Part for the slaves
+	XpathCombination = Xpath + "/Combination/"
+	ListWeaponCombination = "Cannon", "Missile", "Beam"
+	for i in ListWeaponCombination:
+		Damage = GetStats(Tree, XpathCombination + i + "/Damage", BaseDamage)
+		Reload = GetStats(Tree, XpathCombination + i + "/Reload", BaseReload)
+		Range = GetStats(Tree, XpathCombination + i + "/Distance", BaseRange)
+		Speed = GetStats(Tree, XpathCombination + i + "/Speed", BaseSpeed)
+		# Pretty printing slave
+		print ("< " + i + " slave >")
+		print ("[Damage per shot][" + str(Damage) + "]")
+		print ("[Reload time of][" + str(float(Reload)/1000) + "]")
+		print ("[Damage per seconds of][" + str((float(Damage)*1000)/(float(Reload))) + "]")
+		print ("[Range of][" + str(float(Range) * SectorRange) + "]")
+		print ("[Projectile speed of][" + str(Speed) + "]")
+	return
+
+
+def GetStats(Tree, Xpath, BaseValue):
+	Links = Tree.find(Xpath)
+	if (Links.attrib['style'] == "nerf"):
+		Value = float(BaseValue) / float(Links.attrib['value'])
+	elif (Links.attrib['style'] == "buff"):
+		Value = float(BaseValue) * ( 1 + float(Links.attrib['value']))
+	else:
+		Value = BaseValue
+	return Value
+
+if __name__ == "__main__":
+	GetBlockBehaviorConfigWeapons()
