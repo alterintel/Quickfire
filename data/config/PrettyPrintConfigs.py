7c50a777d90820d99d70d6b5f47e72723ef96a47
alterintel <42732115+alterintel@users.noreply.github.com>
2019-02-06 19:27:03 +0900

    Update PrettyPrintConfigs.py

--- data/config/PrettyPrintConfigs.py
+++ data/config/PrettyPrintConfigs.py
@@ -11,7 +11,7 @@ def GetBlocksConfig():
 	# Basic armor
 	Xpath = ".//Basic/Grey/Block"
 	Mass, HP, Armor = GetArmorBlockValue(Tree, Xpath)
-	BasicArmor = "```md< Basic_armor >\n[Mass][" + Mass + "]\n[Hitpoints][" + HP + "]\n[ArmorValue][" + Armor + "]"
+	BasicArmor = "```md\n< Basic_armor >\n[Mass][" + Mass + "]\n[Hitpoints][" + HP + "]\n[ArmorValue][" + Armor + "]"
 	# Standard armor
 	Xpath = ".//Standard/Grey/Block"
 	Mass, HP, Armor = GetArmorBlockValue(Tree, Xpath)
