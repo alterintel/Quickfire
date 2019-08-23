9e204c0a02340218a1cf41baf3afa0cb8f405dbf
Ithirahad <Ithirahad_IvrarKiim@users.noreply.github.com>
2019-08-23 17:31:55 -0400

    Updated to dev; Added Zoolimar's armor values
    Testing new armor configuration.

--- data/config/ArmorStatChange.py
+++ data/config/ArmorStatChange.py
@@ -7,9 +7,10 @@ def run():
 	Tree.parse("BlockConfig.xml")
 
 	# Basic armor
-	Hp = "200"
+	Hp = "400"
 	Armor = "0"
-	Mass = "0.025"
+	Mass = "0.05"
+	Description = "Cheap but resilient structural blocks."
 	ListBasicArmor = "Grey", "White", "DarkGrey", "Black", "Yellow", "Orange", "Red", "Pink", "Purple", "Blue", "Teal", "Green", "Brown", "Glass"
 	for i in ListBasicArmor:
 		Xpath = ".//Basic/" + i + "/Block/Hitpoints"
@@ -18,14 +19,19 @@ def run():
 		ReplaceEverythingInPath(Tree, Xpath, Armor)
 		Xpath = ".//Basic/" + i + "/Block/Mass"
 		ReplaceEverythingInPath(Tree, Xpath, Mass)
+		Xpath = ".//Basic/" + i + "/Block/Description"
+		ReplaceEverythingInPath(Tree, Xpath, Description)
+	#Basic Doors
+	Mass = "0.05"
 	ReplaceEverythingInPath(Tree, ".//Doors/Basic/Block/Hitpoints", Hp)
 	ReplaceEverythingInPath(Tree, ".//Doors/Basic/Block/ArmorValue", Armor)
 	ReplaceEverythingInPath(Tree, ".//Doors/Basic/Block/Mass", Mass)
 
 	# Standard armor
-	Hp = "1500"
-	Armor = "100"
-	Mass = "0.05"
+	Hp = "200"
+	Armor = "4.4"
+	Mass = "0.1"
+	Description = "Factory processing of basic armor blocks with special hardening compound yields Standard Armor. Standard Armor has less HP per block, but has an internal structure which resists more damage from projectiles and beam weapons when stacked in layers."
 	ListArmorColor = "Grey", "White", "DarkGrey", "Black", "Yellow", "Orange", "Red", "Pink", "Purple", "Blue", "Teal", "Green", "Brown"
 	for i in ListArmorColor:
 		Xpath = ".//Standard/" + i + "/Block/Hitpoints"
@@ -34,17 +40,25 @@ def run():
 		ReplaceEverythingInPath(Tree, Xpath, Armor)
 		Xpath = ".//Standard/" + i + "/Block/Mass"
 		ReplaceEverythingInPath(Tree, Xpath, Mass)
+		Xpath = ".//Standard/" + i + "/Block/Description"
+		ReplaceEverythingInPath(Tree, Xpath, Description)
+	Description = "Further processing of basic armor blocks yields Standard Armor. Standard Armor has less HP per block, but has an internal structure which resists more damage from projectiles and beam weapons when stacked. These specially-painted Standard Armor blocks have been emblazoned with a caution stripe pattern."
 	ReplaceEverythingInPath(Tree, ".//Hazard/Block/Hitpoints", Hp)
-	ReplaceEverythingInPath(Tree, ".//Doors/Standard/Block/Hitpoints", Hp)
 	ReplaceEverythingInPath(Tree, ".//Hazard/Block/ArmorValue", Armor)
-	ReplaceEverythingInPath(Tree, ".//Doors/Standard/Block/ArmorValue", Armor)
 	ReplaceEverythingInPath(Tree, ".//Hazard/Block/Mass", Mass)
+	ReplaceEverythingInPath(Tree, ".//Hazard/Block/Description", Description)
+	
+	# Standard Equiv. Doors
+	Mass = "0.15"
+	ReplaceEverythingInPath(Tree, ".//Doors/Standard/Block/Hitpoints", Hp)
+	ReplaceEverythingInPath(Tree, ".//Doors/Standard/Block/ArmorValue", Armor)
 	ReplaceEverythingInPath(Tree, ".//Doors/Standard/Block/Mass", Mass)
 
 	# Advanced armor
-	Hp = "2000"
-	Armor = "250"
-	Mass = "0.1"
+	Hp = "100"
+	Armor = "15.63"
+	Mass = "0.2"
+	Description = "Further processing and hardening of Standard Armor results in Advanced Armor. While seemingly very brittle in terms of hitpoints, Advanced Armor's hardened composition gives much greater reinforcement per block when layered."
 	ListArmorColor = "Grey", "White", "DarkGrey", "Black", "Yellow", "Orange", "Red", "Pink", "Purple", "Blue", "Teal", "Green", "Brown"
 	for i in ListArmorColor:
 		Xpath = ".//Advanced/" + i + "/Block/Hitpoints"
@@ -53,6 +67,10 @@ def run():
 		ReplaceEverythingInPath(Tree, Xpath, Armor)
 		Xpath = ".//Advanced/" + i + "/Block/Mass"
 		ReplaceEverythingInPath(Tree, Xpath, Mass)
+		Xpath = ".//Advanced/" + i + "/Block/Description"
+		ReplaceEverythingInPath(Tree, Xpath, Description)
+	# Advanced Doors
+	Mass = "0.3"
 	ReplaceEverythingInPath(Tree, ".//Doors/Advanced/Block/Hitpoints", Hp)
 	ReplaceEverythingInPath(Tree, ".//Doors/Advanced/Block/ArmorValue", Armor)
 	ReplaceEverythingInPath(Tree, ".//Doors/Advanced/Block/Mass", Mass)
